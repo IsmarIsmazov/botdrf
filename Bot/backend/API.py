0c528871747487baa93f4af2fe92fe2078c17929
Ismar <ismar110501@gmail.com>
2023-09-25 20:46:07 +0600

    nginx add

--- Bot/backend/API.py
+++ Bot/backend/API.py
@@ -1,2 +1,2 @@
-PRODUCT_API_URL_RANDOM = "http://0.0.0.0:8000/random/"
-PRODUCT_API_URL_LIST = "http://0.0.0.0:8000/products/"
+PRODUCT_API_URL_RANDOM = "https://0.0.0.0:8000/random/"
+PRODUCT_API_URL_LIST = "https://0.0.0.0:8000/products/"
